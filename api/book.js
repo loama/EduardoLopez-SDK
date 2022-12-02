0a276e70ef6c79fbb050a2d994d0aa15b016b7e2
Jaime López <5743857+loama@users.noreply.github.com>
2022-12-02 10:00:05 -0600

    connect to first endpoint, create 404 catch all and organize index.js

--- api/book.js
+++ api/book.js
@@ -1,1 +1,9 @@
-// import axios from 'axios'
+const axios = require('axios')
+
+async function getAllBooks (req, res) {
+  const books = await axios.get(process.env.API_BASE + 'book')
+  console.log(books.data)
+  res.send(books.data)
+}
+
+module.exports = { getAllBooks }
--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
-app.get('/', function (req, res) {
-  console.log(process.env.API_BASE)
-  res.send({ a: 'Hello from express' })
-})
+// Modules
+const books = require('./book')
 
-app.listen(3000, () => {
-  console.log('Server started in port 3000')
-})
+app
+  .get('/', (req, res) => {
+    res.send('Welcome to the LOTR Eduardo SDK')
+  })
+  .get('/books', (req, res) => books.getAllBooks(req, res))
+  .all('*', (req, res) => {
+    res.status('404').send('Route not found or incorrect HTTP method, please check and try again')
+  })
+  .listen(3000, () => {
+    console.log('Server started in port 3000')
+  })
