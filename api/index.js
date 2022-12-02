d13818167e2b95b543754d40807984906fb5f93e
Jaime López <5743857+loama@users.noreply.github.com>
2022-12-02 09:37:00 -0600

    add eslint and environment variables configuration

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
-const express = require("express")
+const express = require('express')
 const app = express()
+require('dotenv').config()
 
 app.get('/', function (req, res) {
-  res.send({a: 'Hello from express'})
+  console.log(process.env.API_BASE)
+  res.send({ a: 'Hello from express' })
 })
 
 app.listen(3000, () => {
-  console.log("Server started in port 3000")
+  console.log('Server started in port 3000')
 })
